56aaf180a0165edbbb066a24610830142ca9805f
frida stenberg <fridastenberg97@hotmail.com>
2021-05-24 13:38:07 +0200

    frida

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route } from 'react-router-dom';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      
+
       </BrowserRouter>
     </div>
   );
