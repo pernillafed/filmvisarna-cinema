98400815b4501bbfde7e378f7babcb0b8fb5bb2f
frida stenberg <fridastenberg97@hotmail.com>
2021-05-24 14:01:59 +0200

    added schema

--- api/models/screens.js
+++ api/models/screens.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const screenSchema = new Schema({
-
+    name: String,
+    rows: [String],
+    seatsPerRow: {String}
 });
 
 const Screen = mongoose.model("Screen", screenSchema);
