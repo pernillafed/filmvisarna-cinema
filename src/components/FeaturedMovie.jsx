4d9eadb69f67ca0c1dcc00cfe03621b881da28ff
Pernilla <coolanilla@hotmail.com>
2021-05-25 10:48:38 +0200

    Created FeaturedMovie component and added it to MovieList page
    Also added basic styles

---
+++ src/components/FeaturedMovie.jsx
@@ -0,0 +1,7 @@
+function FeaturedMovie() {
+    return (
+        <h1>Featured movie works</h1>
+    )
+}
+
+export default FeaturedMovie;
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,16 +1,20 @@
+import styles from '../css/MovieList.module.css';
+import FeaturedMovie from '../components/FeaturedMovie';
+
 function MovieList() {
     return (
         <div className="container">
             <div className="header">
-                <h2>I fokus</h2>
+                <h2 className={styles.heading}>I fokus</h2>
+                <FeaturedMovie />
             </div>
             <div className="movie-list">
-                <h2>Alla filmer</h2>
-                <div className="filter-line">
+                <h2 className={styles.heading}>Alla filmer</h2>
+                <div className={styles.filterLine}>
                     <h3>På bio nu</h3>
                     <a href="#">Visa filter</a>
                 </div>
-                <hr />
+                {/* MovieList component here */}
             </div>
         </div>
     )
