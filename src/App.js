d998a315da42aa6764d6ef9286b818c2bf203024
Pernilla <coolanilla@hotmail.com>
2021-05-25 10:04:26 +0200

    Created MovieList page and added it to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import React from "react";
 
+import MovieList from './pages/MovieList';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-
+        <Route exact path="/movie-list" component={MovieList} />
       </BrowserRouter>
     </div>
   );
---
+++ src/pages/MovieList.jsx
@@ -0,0 +1,7 @@
+function MovieList() {
+    return (
+        <h1>Movie list works</h1>
+    )
+}
+
+export default MovieList;
