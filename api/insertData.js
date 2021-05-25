3d877cf5a5ee5332f57ce248440b2b29890b1835
Chris <to.chris.lindstrom@gmail.com>
2021-05-25 10:02:15 +0200

    Data added to DB, added comments to inserData.js

--- api/insertData.js
+++ api/insertData.js
@@ -3,20 +3,23 @@ const Movie = require('./models/Movie');
 const uri = 'mongodb+srv://dbTest:dbTestPassword@cluster0.gneaa.mongodb.net/cinemaDB?retryWrites=true&w=majority';
 const movieData = require('./data/movieData.json');
 
+// Run this page with 'node insertData' in the terminal
+// Can add different data by importing another json-file above and changing the model in the 'insertData'-function below
+// Run page only once with the new data
+
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
   console.log("MongoDb connected")
   insertData();
-  console.log(movieData);
 }).catch((err) => {
   console.log(err)
 })
 
 async function insertData() {
   console.log('Data is being stored...');
-  await Movie.create(movieData);
+  // await Movie.create(movieData);
   console.log('Data stored...')
   mongoose.connection.close();
 }
